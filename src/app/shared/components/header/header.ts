cd85f12f638126ebb7ce97bae6c1d9d797b64d0c
Jan Holtschke <60309436+BigOzzyOz@users.noreply.github.com>
2025-10-22 16:54:54 +0200

    feat: header component improvements small devices

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { RouterLinkActive, RouterLink } from "@angular/router";
+import { Parallax } from '../../services/parallax';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLinkActive, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+  parallax = inject(Parallax);
+
+  burgerMenuClosed = signal(true);
+
+  toggleBurgerMenu() {
+    this.burgerMenuClosed.update(value => !value);
+  }
 
 }
