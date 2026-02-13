bcbb74b162f729038270e0daa83646d9c035bb5d
Jan Holtschke <60309436+BigOzzyOz@users.noreply.github.com>
2026-02-13 19:13:46 +0100

    feat: enhance title strategy with meta tags for SEO optimization

--- src/app/core/strategies/template-title-strategy.ts
+++ src/app/core/strategies/template-title-strategy.ts
@@ -1,10 +1,11 @@
 import { Injectable, inject } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
 import { TitleStrategy, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class TemplatePageTitleStrategy extends TitleStrategy {
     private readonly title = inject(Title);
+    private readonly meta = inject(Meta);
 
     override updateTitle(routerState: RouterStateSnapshot): void {
         const title = this.buildTitle(routerState);
@@ -12,7 +13,9 @@ export class TemplatePageTitleStrategy extends TitleStrategy {
             this.title.setTitle(`Napata | ${title}`);
         }
         else {
-            this.title.setTitle('Napata');
+            this.title.setTitle('Napata – Survival, Wildnis und indigene Pädagogik');
         }
+        this.meta.updateTag({ name: 'description', content: 'Erlebe die Natur wie indigene Gemeinschaften: Lerne Survival, Wildnispädagogik und Naturhandwerk. Stärke Gemeinschaft, Identität & Stressabbau.' });
+        this.meta.updateTag({ name: 'keywords', content: 'wildnispädagogik, survival, survival training, survivel, survival urlaub, outdoor survival camp, wildnispädagogik ausbildung, wildnispädagogik, wildnisschule, wildnispädagogik ausbildung, wildnispädagogik'});
     }
 }
