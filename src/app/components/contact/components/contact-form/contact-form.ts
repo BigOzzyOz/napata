de25f7b73cfdaf47b7c83507374634a9340a0289
Jan Holtschke <60309436+BigOzzyOz@users.noreply.github.com>
2026-03-01 01:13:15 +0100

    feat: enhance contact form with reactive validation and privacy acceptance

--- src/app/components/contact/components/contact-form/contact-form.ts
+++ src/app/components/contact/components/contact-form/contact-form.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.scss',
 })
 export class ContactForm {
-  [x: string]: any;
+  private http = inject(HttpClient);
+
+  contactForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    firstName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    subject: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    message: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    privacyAccepted: new FormControl(false, Validators.requiredTrue)
+  });
+
+  get name(): FormControl {
+    return this.contactForm.get('name') as FormControl;
+  }
+
+  get firstName(): FormControl {
+    return this.contactForm.get('firstName') as FormControl;
+  }
+
+  get email(): FormControl {
+    return this.contactForm.get('email') as FormControl;
+  }
+
+  get subject(): FormControl {
+    return this.contactForm.get('subject') as FormControl;
+  }
+
+  get message(): FormControl {
+    return this.contactForm.get('message') as FormControl;
+  }
+
+  get privacyAccepted(): FormControl {
+    return this.contactForm.get('privacyAccepted') as FormControl;
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      const formData = this.contactForm.value;
+      const url = environment.phpUrl;
+
+      this.http.post(url, formData).subscribe({
+        next: (response) => {
+          console.log('Daten wurden übergeben', response);
+          this.contactForm.reset();
+        },
+        error: (error) => {
+          console.error('Es gab ein Problem bei der Übertragung', error);
+        }
+      });
+    }
+  }
+
 
 }
